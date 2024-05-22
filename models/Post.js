0b13375f6fd736729b0efc1aa26449377d8633e2
yahm0 <stnt190@outlook.com>
2024-05-22 19:37:47 -0400

    added code to the Post.js

--- models/Post.js
+++ models/Post.js
@@ -19,3 +19,29 @@ Post.init(
       allowNull: false,
     },
    
+
+     // Define the content column
+     content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      // Reference to the User model
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+    {
+      // Model configuration
+      sequelize,
+      timestamps: true, // Automatically add createdAt and updatedAt fields
+      freezeTableName: true, // Prevent Sequelize from renaming the table
+      underscored: true, // Use snake_case for automatically added fields
+      modelName: 'post', // Name of the model
+    }
+  );
+  
+  module.exports = Post;
