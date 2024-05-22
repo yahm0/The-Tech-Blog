151425c903b7a623e11de50ffbe574629ab785a6
yahm0 <stnt190@outlook.com>
2024-05-22 19:39:31 -0400

    added code to define the User model.

--- models/User.js
+++ models/User.js
@@ -8,3 +8,38 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
+
+// Define the User model
+User.init(
+    {
+      // Define the ID column
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      // Define the username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      // Define the password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      // Hooks to hash the password before saving or updating
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+      },
