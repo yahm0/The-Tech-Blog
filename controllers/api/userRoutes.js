c003fc32ab0b387a0ac5d1010547e51715e7aa83
yahm0 <stnt190@outlook.com>
2024-05-22 19:30:52 -0400

    added logic to the userRoutes file that adds a route to log in an existing user.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,3 +14,30 @@ router.post('/signup', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+// Route to log in an existing user
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { username: req.body.username } });
+      if (!userData) {
+        res.status(400).json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      const validPassword = userData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
