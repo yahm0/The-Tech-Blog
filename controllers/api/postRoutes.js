5b46464361953aa36443cb059ee32072a1568901
yahm0 <stnt190@outlook.com>
2024-05-22 19:28:20 -0400

    added logic to the postRoute file that creates a new post.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Route to create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
