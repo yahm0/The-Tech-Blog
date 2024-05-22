1c72afca6a8a69c51e5d76d36e8cdbfea7d35fee
yahm0 <stnt190@outlook.com>
2024-05-22 19:26:55 -0400

    added logic to the comment Routes.js and also added logic to the server.js file establishing my seesions and constants.

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
