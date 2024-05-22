65d76cd8be3a6ede4fb1380844f6828c216856bc
yahm0 <stnt190@outlook.com>
2024-05-22 19:27:39 -0400

    added logic to the server.js file so the server can use routes and init correctly.

--- server.js
+++ server.js
@@ -21,3 +21,18 @@ const sess = {
     db: sequelize,
   }),
 };
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+});
