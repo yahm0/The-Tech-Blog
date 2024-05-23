e6e1ad523228ba53ebe6e468e906ffd63b7be9fb
yahm0 <stnt190@outlook.com>
2024-05-22 20:30:56 -0400

    Fix module import paths

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers'); // Ensure this path is correct
+const routes = require('./controllers'); 
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;  // Use the PORT provided by Heroku
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);  // Ensure this line is correctly pointing to your routes
+app.use(routes); 
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
