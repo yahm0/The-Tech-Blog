6439dd9ba796aa8a293162d9de46171a54439f9d
yahm0 <stnt190@outlook.com>
2024-05-22 20:44:22 -0400

    Fix import paths and ensure correct directory structure

--- models/Comments.js
+++ models/Comments.js
@@ -3,22 +3,18 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-// Define the Comment model
 Comment.init(
   {
-    // Define the ID column
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    // Define the comment text column
     comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // Reference to the User model
     user_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -26,7 +22,6 @@ Comment.init(
         key: 'id',
       },
     },
-    // Reference to the Post model
     post_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -36,12 +31,10 @@ Comment.init(
     },
   },
   {
-    // Model configuration
     sequelize,
-    timestamps: true, // Automatically add createdAt and updatedAt fields
-    freezeTableName: true, // Prevent Sequelize from renaming the table
-    underscored: true, // Use snake_case for automatically added fields
-    modelName: 'comment', // Name of the model
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
   }
 );
 
