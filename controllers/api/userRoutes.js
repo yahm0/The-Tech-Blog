0bfef9c2ee5624358337d1902a367bbed3d0aadb
yahm0 <stnt190@outlook.com>
2024-05-23 00:43:59 -0400

    made a change to the routes.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,7 +14,7 @@ router.post('/signup', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = newUser.id;
             req.session.logged_in = true;
-            res.json(newUser);
+            res.status(200).json(newUser);
         });
     } catch (err) {
         res.status(500).json(err);
@@ -58,5 +58,26 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
+
+router.post('/signup', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.logged_in = true;
+            res.status(200).json(newUser);
+        });
+    } catch (err) {
+        console.error(err); // Log the error
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
