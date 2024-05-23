9daae4a1fac922b5b7d67a54df59b5a6ba8ecd02
yahm0 <stnt190@outlook.com>
2024-05-22 20:39:59 -0400

    added many things to get this to work on heroku

--- models/User.js
+++ models/User.js
@@ -1,55 +1,47 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection'); // Ensure this path is correct
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
 
 class User extends Model {
-  // Method to check password validity
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// Define the User model
 User.init(
-    {
-      // Define the ID column
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
-      // Define the username column
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      // Define the password column
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
     },
-    {
-      // Hooks to hash the password before saving or updating
-      hooks: {
-        beforeCreate: async (newUserData) => {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
-        },
-        beforeUpdate: async (updatedUserData) => {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
-        },
-      },
-
-      // Model configuration
     sequelize,
-    timestamps: false, // Disable automatic addition of createdAt and updatedAt fields
-    freezeTableName: true, // Prevent Sequelize from renaming the table
-    underscored: true, // Use snake_case for automatically added fields
-    modelName: 'user', // Name of the model
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
   }
 );
 
